7a71670500fe8b7b23f38c671a400decde62be86
Agam-gupTa-1 <curiousagam@gmail.com>
2022-10-01 17:05:06 +0530

    Update 91. Decode Ways.cpp

--- 91. Decode Ways.cpp
+++ 91. Decode Ways.cpp
@@ -24,7 +24,23 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n = s.size();
-        vector<int> dp(n, -1);
-        return solve(0, n, s, dp);
+        vector<int> dp(n+1, 0);
+        // return solve(0, n, s, dp);
+        dp[n] = 1;
+        
+        for(int i = n-1; i >= 0; i--)
+        {
+            int ch = 0;
+            int ways = 0;
+            for(int j = i; j < n; j++)
+            {
+                ch = ch*10 + (s[j]-'0');
+                if(ch == 0) break;
+                if(ch > 26 ) break;
+                ways += dp[j+1];
+            }
+            dp[i] = ways;
+        }
+        return dp[0];
     }
 };
